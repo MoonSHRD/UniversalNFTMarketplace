e0347786cfd07e911473a0a61d74f9c88a18af7a
Bekket <jackatwork@yandex.ru>
2021-08-18 17:58:22 +0300

    deployer proto functions

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,17 @@
 var MasterFactory = artifacts.require("./721/singleton/MasterFactory721.sol")
 //var Deposit = artifacts.require("./Deposit.sol")
 var Master = artifacts.require("./721/singleton/MSNFT.sol")
+
 var accounts = web3.eth.getAccounts();
 var limitGas = web3.eth.getBlock("latest").gasLimit;
+
+
+// Test dummy erc20 tokens
+var USDT = artifacts.require("./721/singleton/TestTokenERC20.sol")
+var USDC = artifacts.require("./721/singleton/TestTokenERC20.sol")
+var DAI = artifacts.require("./721/singleton/TestTokenERC20.sol")
+var WETH = artifacts.require("./721/singleton/TestTokenERC20.sol")
+var SNM = artifacts.require("./721/singleton/TestTokenERC20.sol")
 
 //var deposit_value = '50000';  // deposit INITIAL exchange market cup (turn capital)
 //var deposit_value_wei = web3.utils.toWei(deposit_value,'ether');
@@ -19,20 +28,20 @@ module.exports = function(deployer, network, accounts) {
   console.log(accounts);
   console.log(wei_gas_price);
  // console.log(string_gas_price);
+
+ /*
   deployer.deploy(MasterFactory,accounts[1],{gasPrice: wei_gas_price, from:accounts[0]}).then(function() {
-  console.log(network);
-  console.log(network.port);
-  console.log(network.gasPrice);
-  console.log(accounts);
-  console.log("deployer");
-  console.log(accounts[0]);
-  console.log("treasure");
-  console.log(accounts[1]);
-  console.log("gas limit");
-  console.log(limitGas);
+ 
   return;
  // return deployer.deploy(SuperFactory, KNS.address,accounts[1],accounts[2],{gasPrice:'1'});
 });
+*/
+
+await deployer.deploy(USDT);
+await deployer.deploy(USDC);
+await deployer.deploy(DAI);
+await deployer.deploy(WETH);
+await deployer.deploy(SNM);
 
 
 
