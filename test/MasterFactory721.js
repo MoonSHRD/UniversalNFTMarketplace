25f2416bcc68e3d60213a4f55d45959c5c54178f
Bekket <jackatwork@yandex.ru>
2021-09-15 15:43:38 +0300

    event fixed, tokensale address fixed

--- test/MasterFactory721.js
+++ test/MasterFactory721.js
@@ -167,7 +167,7 @@ contract('MasterFactory721', accounts => {
 		assert.equal(receiptItemSale.receipt.logs[1].event, 'SaleCreatedHuman', 'should be the SaleCreatedHuman event');
         assert(userTokenBalanceAfter >= receiptItemSale.receipt.logs[0].args.price);
 
-        const contractAddress = receiptItemSale.receipt.logs[0].address;    // wrong address (it should be tokensale address, not factory)
+        const contractAddress = receiptItemSale.receipt.logs[0].args.it_sale;    // wrong address (it should be tokensale address, not factory)
         console.log(contractAddress);
         tokenSale721 = await TokenSale721.at(contractAddress);
         await debug(tokenSale721.buyTokens(user, 1, USDT));
