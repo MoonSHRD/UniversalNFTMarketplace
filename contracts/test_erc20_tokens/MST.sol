9f232819741e7b83dfb0bd333bbd94d54ee0e01b
Takeshi <takeshi.hokugava@gmail.com>
2021-12-23 11:38:31 -0800

    returned mst mint function

--- contracts/test_erc20_tokens/MST.sol
+++ contracts/test_erc20_tokens/MST.sol
@@ -7,7 +7,14 @@ import "./TestTokenERC20.sol";
  contract MST is TestTokenERC20 {
 
     constructor(string memory name_, string memory symbol_) TestTokenERC20(name_,symbol_) {}
-
+/*
+ @TODO: delete the function below before release
+ 
+ LINES 15-17 ARE HERE FOR TESTING PURPOSES ONLY! IT'S IMPORTANT TO REMOVE THEM AFTERWARDS!
+ */
+    function MintERC20 (address to, uint amount) public override{
+        super._mint(to, amount);
+    }
 }
 
  
