74a60e249224152a029b382bc82e983c5795bee7
daseinsucks <alexstepov97@gmail.com>
2022-03-29 16:03:01 +0300

    add hardcoded interface check

--- contracts/721/singleton/Judge.sol
+++ contracts/721/singleton/Judge.sol
@@ -5,6 +5,8 @@ import "../../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import './MSNFT.sol';
 import './InterfaceRegister.sol';
 import '../../test_erc20_tokens/BlackMark.sol';
+import "../../../node_modules/@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+
 
 contract Judge is Ownable, InterfaceRegister {
 
@@ -14,12 +16,19 @@ contract Judge is Ownable, InterfaceRegister {
 
     function check(address _licenseKeeper, address _nft) public onlyOwner() returns (bool) {
         address master_adr = _nft;
+        bytes4[] memory interfaceIds = new bytes4[](3);
+        interfaceIds[0] = 0x5d08e584;
+        interfaceIds[1] = 0x958c8917;
+        interfaceIds[2] = 0x033a36bd;
+        interfaceIds[3] = 0xeaf92d7a;
+        bool usingOurInterfaces = ERC165Checker.supportsAllInterfaces(_licenseKeeper, interfaceIds);
+
         require(master_adr != address(0), "MSNFT address equal 0x0");
         MSNFT master = MSNFT(master_adr);
         uint[] memory mid = master.getMasterIdByAuthor(_licenseKeeper);
-        if(mid.length > 0 && mid[0] == 1){
+        if (mid[0] == 1){
             for(uint i = 0; i < mid.length; i++) {
-                if(mid[i] == 1) {
+                if(mid[i] == 1 && usingOurInterfaces == true) {
                     emit Check(_licenseKeeper, mid);
                     return true;
                 }
